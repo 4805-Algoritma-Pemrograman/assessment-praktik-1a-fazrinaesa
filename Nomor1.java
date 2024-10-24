250519d834ecb3e9d816e8bcc8c2a07b5c48c4a2
fazrinaesa <fazrinaesa2005@gmail.com>
2024-10-24 07:57:03 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Nomor1{
     public static void main(String[] args){
+        int jamLembur;
+        double honorLembur;
+        Scanner sc = new Scanner(System.in);
+        
 
+        System.out.print("Masukkan total JamLembur: ");
+       int durasiLembur = sc.nextInt();
+         
+       double HonorLembur;
+        if (durasiLembur <+ 16) {
+            honorLembur = durasiLembur * 10000;
+        } else {
+            honorLembur = 16 * 10000 + (durasiLembur - 16) * 15000;
+        }
+        System.out.println("Total honor lembur: " + honorLembur);
+       
     }
+
 }
+
+Psedocode 
+Program MenghitungJumlahHonor;
+Deklarasi 
+    int jamLembur;
+    double honorLembur;
+
+Algoritma 
+    Menghitung durasi lembur
+    Memasukan honor lembur
+    Menghasilkan total honor lembur
+END 
